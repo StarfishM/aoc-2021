e3fcb80b38e682a9dc72252b7203eb23333af99a
Merle Fischer <merle_canada@hotmail.com>
2021-12-03 17:27:47 +0100

    adds wacky solution for part 2 🙈 way toooooo much looping going on...

---
+++ README.md
@@ -0,0 +1,17 @@
+<style>
+    h1,h2{
+        color:#4F4E4D;
+    }
+</style>
+<h1 style="align:center;">Advent of Code 2021</h1>
+
+<p align="center">
+    <a title="Advent of Code" href="https://adventofcode.com/2021">
+        <img alt="Advent of Code Logo" src="advent_of_code_logo.png" width="100"/>
+    </a>
+</p>
+----
+## Puzzles
+
+-   ⭐️⭐️ [Day 1: Sonar Sweep](./d-1) --> [[input](./d-1/input.js), [solution](./d-1/index.js)]
+-   ⭐️⭐️ [Day 2: Dive!](./d-2) --> [[input](./d-2/input.js), [solution](./d-2/index.js)]
--- d-3/index-2.js
+++ d-3/index-2.js
@@ -1,3 +1,16 @@
+/*
+Considering only the first bit of each number, there are five 0 bits and seven 1 bits. Since the most common bit is 1, the first bit of the gamma rate is 1.
+
+The most common second bit of the numbers in the diagnostic report is 0, so the second bit of the gamma rate is 0.
+
+The most common value of the third, fourth, and fifth bits are 1, 1, and 0, respectively, and so the final three bits of the gamma rate are 110.
+
+So, the gamma rate is the binary number 10110, or 22 in decimal.
+
+The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used. So, the epsilon rate is 01001, or 9 in decimal. Multiplying the gamma rate (22) by the epsilon rate (9) produces the power consumption, 198.
+
+Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together. What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)*/
+
 const { testInput, input } = require("./input");
 
 const findMostOccuring = (arr) => {
@@ -38,26 +51,34 @@ const epsilonNum = parseInt(epsilonBin, 2);
 console.log("part 1 ------ ", gammaNum * epsilonNum);
 
 // PART 2
-// upper function to find most occurence couldn't properly handle same amount of 0s and 1s...
-const matchNums = (str) => str.match(/(1*)(0*)/g);
-
-const findMostOccReg = (arr) => {
-    if (!arr) return;
-    let [numOfZero, numOfOne] = matchNums(arr.sort((a, b) => a - b).join(""));
-    numOfZero = numOfZero?.length;
-    numOfOne = numOfOne?.length;
-    return numOfOne >= numOfZero ? 1 : 0;
-};
+// upper function to find most occurence couldn't properly handle same amount...
 const findLeastOccReg = (arr) => {
     if (!arr) return;
-    let [numOfZero, numOfOne] = matchNums(arr.sort((a, b) => a - b).join(""));
-
-    numOfZero = numOfZero?.length;
-    numOfOne = numOfOne?.length;
+    const numOfOne = arr
+        .sort((a, b) => a - b)
+        .join("")
+        .match(/[1*]/g)?.length;
+    const numOfZero = arr
+        .sort((a, b) => a - b)
+        .join("")
+        .match(/[0*]/g)?.length;
     return numOfZero <= numOfOne ? 0 : 1;
 };
 
-const filterMost = (arr) => {
+const findMostOccReg = (arr) => {
+    if (!arr) return;
+    const numOfOne = arr
+        .sort((a, b) => a - b)
+        .join("")
+        .match(/[1*]/g)?.length;
+    const numOfZero = arr
+        .sort((a, b) => a - b)
+        .join("")
+        .match(/[0*]/g)?.length;
+    return numOfOne >= numOfZero ? 1 : 0;
+};
+
+const createFilterO2 = (arr) => {
     const filterArr = [];
     arr.forEach((each) => {
         const colBits = each.split("");
@@ -65,7 +86,7 @@ const filterMost = (arr) => {
     });
     return filterArr;
 };
-const filterLeast = (arr) => {
+const createFilterCO2 = (arr) => {
     const filterArr = [];
     arr.forEach((each) => {
         const colBits = each.split("");
@@ -73,13 +94,13 @@ const filterLeast = (arr) => {
     });
     return filterArr;
 };
-const o2Filter = filterMost(arrOfColVals);
-const co2Filter = filterLeast(arrOfColVals);
+const o2Filter = createFilterO2(arrOfColVals);
+const co2Filter = createFilterCO2(arrOfColVals);
 
 const filterInputO2 = (arr, filterArr, i = 0) => {
     const filtered = arr.filter((each) => each[i] === filterArr[i]);
     let newFilterArr = [];
-    newFilterArr = filterMost(createColArr(filtered));
+    newFilterArr = createFilterO2(createColArr(filtered));
     if (filtered.length === 1) {
         return filtered[0];
     } else {
@@ -90,7 +111,7 @@ const filterInputO2 = (arr, filterArr, i = 0) => {
 const filterInputCO2 = (arr, filterArr, i = 0) => {
     const filtered = arr.filter((each) => each[i] === filterArr[i]);
     let newFilterArr = [];
-    newFilterArr = filterLeast(createColArr(filtered));
+    newFilterArr = createFilterCO2(createColArr(filtered));
     if (filtered.length === 1) {
         return filtered[0];
     } else {
